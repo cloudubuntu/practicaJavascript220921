39e891d4d8cab8fbd03adce938c88b2246dac72e
alanChibilisco <alan.chibilisco@gmail.com>
2021-09-22 18:36:49 -0300

    primera clase de git 22-09-21

--- js/1-holaMundo.js
+++ js/1-holaMundo.js
@@ -1,3 +1,60 @@
-let str="Hola Mundo";
+//esto es un comentario
 
-print(str);
+/*comentario de varias lineas*/
+
+//mensaje en la consola
+console.log ("Hola Mundo");
+console.log ("esto es otro mensaje");
+
+//mostrar un msj en el documento html
+document.write("hola mundo documento");
+
+//mostrar un msj en una ventana emergente
+//alert("esto es una alerta");
+
+
+// declaracion de variables (var, let, const)
+
+var name="alan";
+
+let surnanme="chibilisco";
+
+let dni="33703015";
+
+function imprimeConsola(){
+    return console.log(name+" "+ surnanme + " dni n° "+ dni);
+}
+
+
+document.write("<br>Datos:"+name+" "+surnanme+" dni n° "+dni);
+
+let alumno="Rodrigo";
+
+document.write("<br>Nombre:"+alumno);
+
+alumno="Mata";
+let edad=33;
+
+document.write("<br>Nombre:"+alumno);
+
+//pedir a un usuario que ingrese un nombre de un alumno y almacenarlo en la variable correspondiente
+
+alumno = prompt("Ingrese el nombre del alumno/a");//sirve para crear un input
+edad = prompt("Ingrese la edad del alumno/a");//sirve para crear un input
+
+document.write("<br>Nombre:"+alumno);
+document.write("<br>Edad:"+edad);
+
+const url="https://campus.rollingcodeschool.com/mod/forum/view.php?id=10310";
+
+document.write("<br>url:"+url);
+
+//url="prueba";
+
+document.write("<br>uel:"+url);
+
+let numero1=20;
+let numero2=parseInt(prompt("ingrese un numero"));
+
+
+document.write("<br>Resultado:"+(numero1+numero2 ));
